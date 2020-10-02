c3b5fd4cceeb857e36e18db48fb83ee50e47fca9
Aliaksei <alex.latypov94@gmail.com>
2020-10-02 11:57:11 +0300

    feat: add task solution brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let newBrack = []
+  let newStr = str
+  bracketsConfig.forEach(el => {
+    newBrack.push(el.join(''))
+  });
+  
+  long = str.length
+      for (let j = 0; j < newBrack.length; j++) {
+
+        if(str.includes(newBrack[j])) {
+          newStr = str.replace(newBrack[j], "")
+        }
+      }
+      
+  console.log(str)
+    if(newStr === str) {
+      return false
+    } else if(newStr === "") {
+      return true
+    }else {
+    return check(newStr, bracketsConfig)
+    }
 }
